6e2db9c0ea94c6f799224b979b2c754deef0ec46
yukajacso0420 <bhpiuggcw9785@hotmail.com>
2026-03-10 20:30:57 +0800

    Add PyTorch environment test script

---
+++ matrix_test.py
@@ -0,0 +1,7 @@
+import torch  
+A = torch.tensor([[1.0, 2.0], [3.0, 4.0]])
+B = torch.tensor([[5.0, 6.0], [7.0, 8.0]])
+C = torch.matmul(A, B)
+print("矩阵 A:\n", A)
+print("矩阵 B:\n", B)
+print("运算结果 C = A * B:\n", C)
